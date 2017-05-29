2cf71c9d6f0c4200f34cb50a08726086b4d700f7
Atthar Mirza <atthar.mirza@gmail.com>
2017-05-29 18:01:29 -0400

    Green Trail Alternates Colors
    Instead of randomly alternating the green shades of the trail, it’s now
    directly alternating between dark and light.

--- javascript/exemplumBanner.js
+++ javascript/exemplumBanner.js
@@ -1,50 +1,54 @@
 $(document).ready(function () {
     console.log('hello world!');
-    
+
     d3.xml("graphics/exemplumLogo.svg", function (xml) {
         var htmlSVG = document.getElementById('logoSVG');
         htmlSVG.appendChild(xml.documentElement.getElementById('all'));
-        
+
         var plumTriangles = d3.select('#plumSkin');
-        
-        //d3.select('#plumLetters').style('opacity', '0')
-        
+
         var singleTriangles = plumTriangles.selectAll('polygon');
-        
+
         singleTriangles.on('mouseover', triangleHoverAction);
-        
+
         singleTriangles.on('click', triangleClickAction);
-        
+
+        var greenFillNumber = 1;
+
         function triangleHoverAction() {
-            //console.log('click');
-            //console.log(d3.select(this).attr('class'))
-            
+
             var originalFill = d3.select(this).attr('fill');
-            //console.log(originalFill);
-            
-            function greenFill () {
-                var select = Math.round(Math.random()*2);
-                //console.log('random number is ' + select);
-                
-                if (select < 1) {
-                    return '#7fc88f';
+
+            function greenFill() {
+                //increment through integers 1 to 10
+                if (greenFillNumber < 2) {
+                    greenFillNumber++;
                 } else {
-                    return '#95ce95';
+                    greenFillNumber = 1;
                 };
+                console.log(greenFillNumber);
+                
+                //alternate between green fills
+                if (greenFillNumber % 2 >= 1) {
+                    return '#95ce95'
+                } else {
+                    return '#7fc88f'
+                }
             };
-            
-            //d3.select(this).style('opacity', '0.7').transition().duration(100).transition().duration(500).style('opacity','1');
+
             d3.select(this)
                 .style('fill', greenFill)
-                .transition().duration(300)
+                .transition().duration(100)
                 .transition().duration(500)
                 .style('fill', originalFill);
-            
+
             d3.select(this).style('cursor', 'pointer');
         };
+
+        ;
     });
-    
-    function triangleClickAction () {
+
+    function triangleClickAction() {
         $('#logo').hide();
         $('#content').show();
     };
