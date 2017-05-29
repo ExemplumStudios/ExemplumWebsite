26471a5bf4735686220e40f6527cc8ca35145a2f
Atthar Mirza <atthar.mirza@gmail.com>
2017-05-29 00:01:49 -0400

    Changed from Banner to Logo
    There is now a nice, centered squared up logo that responds to mouse
    over events. Clicking on the plum also links to “index.html” which
    doesn’t exist yet. I might have it trigger a jQuery event.

--- javascript/exemplumBanner.js
+++ javascript/exemplumBanner.js
@@ -1,25 +1,23 @@
 $(document).ready(function () {
     console.log('hello world!');
     
-    d3.xml("graphics/exemplumBanner.svg", function (xml) {
+    d3.xml("graphics/exemplumLogo.svg", function (xml) {
         var htmlSVG = document.getElementById('logoSVG');
         htmlSVG.appendChild(xml.documentElement.getElementById('all'));
         
         var plumTriangles = d3.select('#plumSkin');
-        //plumTriangles.transition().duration(1000).style('opacity', '0.4')
-        /*plumTriangles.each(function () {
-            d3.select(this).transition().duration(1000).style('opacity', '0.4');
-        });*/
         
-        d3.select('#plumLetters').style('opacity', '0')
+        //d3.select('#plumLetters').style('opacity', '0')
         
         var singleTriangles = plumTriangles.selectAll('polygon');
         
-        singleTriangles.on('mouseover', triangleSelectedAction);
+        singleTriangles.on('mouseover', triangleHoverAction);
         
-        function triangleSelectedAction() {
-            console.log('click');
-            d3.select(this).style('opacity', '0').transition().duration(100).transition().duration(500).style('opacity','1');
+        function triangleHoverAction() {
+            //console.log('click');
+            d3.select(this).style('opacity', '0.7').transition().duration(100).transition().duration(500).style('opacity','1');
+            
+            d3.select(this).style('cursor', 'pointer');
         }
     });
 });
