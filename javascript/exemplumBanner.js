2da4dbe243acc7f2603c007d198a6fe17b32a62b
Atthar Mirza <atthar.mirza@gmail.com>
2017-05-29 19:57:10 -0400

    Random Triangle Fills on Tap
    Added some functionality for mobile site, all triangles randomly change
    color if tapped.

--- javascript/exemplumBanner.js
+++ javascript/exemplumBanner.js
@@ -10,31 +10,33 @@ $(document).ready(function () {
         var singleTriangles = plumTriangles.selectAll('polygon');
 
         singleTriangles.on('mouseover', triangleHoverAction);
+        singleTriangles.on('touchstart', triangleTapAction);
 
         singleTriangles.on('click', triangleClickAction);
 
         var greenFillNumber = 1;
 
-        function triangleHoverAction() {
+        function greenFill() {
+            //increment through integers 1 to 10
+            if (greenFillNumber < 2) {
+                greenFillNumber++;
+            } else {
+                greenFillNumber = 1;
+            };
+            console.log(greenFillNumber);
 
+            //alternate between green fills
+            if (greenFillNumber % 2 >= 1) {
+                return '#95ce95'
+            } else {
+                return '#7fc88f'
+            }
+        };
+
+        function triangleHoverAction() {
             var originalFill = d3.select(this).attr('fill');
 
-            function greenFill() {
-                //increment through integers 1 to 10
-                if (greenFillNumber < 2) {
-                    greenFillNumber++;
-                } else {
-                    greenFillNumber = 1;
-                };
-                console.log(greenFillNumber);
-                
-                //alternate between green fills
-                if (greenFillNumber % 2 >= 1) {
-                    return '#95ce95'
-                } else {
-                    return '#7fc88f'
-                }
-            };
+            greenFill;
 
             d3.select(this)
                 .style('fill', greenFill)
@@ -45,11 +47,26 @@ $(document).ready(function () {
             d3.select(this).style('cursor', 'pointer');
         };
 
-        ;
+        function triangleTapAction() {
+            singleTriangles.each(function () {
+                var randomDelay = Math.random() * 1000;
+
+                var originalFill = d3.select(this).attr('fill');
+
+                var singleTriangle = d3.select(this);
+
+                singleTriangle.transition()
+                    .delay(randomDelay)
+                    .style('fill', greenFill)
+                    .transition().duration(100)
+                    .transition().duration(500)
+                    .style('fill', originalFill);
+            });
+        };
     });
 
     function triangleClickAction() {
-        $('#logo').hide();
-        $('#content').show();
+        $('#logo').stop().fadeOut(200);
+        $('#content').delay(200).fadeIn(300);
     };
 });
